1526ecaa87c8317d5efdcd88d3e516dd502a79f7
Ariana <6798854@clases.edu.sv>
2024-11-04 15:32:01 -0600

    Primera version sopon de mario usuarios y login

---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Authenticatable
+{
+    use Notifiable, HasFactory;
+
+    /**
+     * Paginación predeterminada.
+     *
+     * @var int
+     */
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+    // Resto de tu código del modelo User...
+}
+
+/**
+ * Documentación de propiedades del modelo User.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Carbon\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
